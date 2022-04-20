c5beda2fecd831a64027e60be2bc2b4d864657f4
Giovanni Perrotta <gioperrotta@gmail.com>
2022-04-20 20:34:55 -0300

    configuração do Heroku01

--- src/server.js
+++ src/server.js
@@ -4,13 +4,21 @@ import { Router } from 'express'
 
 dotenv.config();
 
+const DATABASE_URL=process.env.DATABASE_URL;
+
 const router = Router();
+
 router.get('/hello', (request, response) => {
   let res = [];
   for (let i = 0; i < 10; i++) {
     res[i] = `${i}-HELLO WORLD !!!`;
   }
   return response.json(res, null, 2)
+})
+
+router.get('/env', (request, response) => {
+
+  return response.json(DATABASE_URL)
 })
 
 const PORT = process.env.PORT
